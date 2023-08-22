b20fbee6e6e0134cf887022874eb1eb7cfe9e18e
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2023-08-22 22:18:17 +0900

    25. user profile

--- lib/features/authentication/view_models/signup_vm.dart
+++ lib/features/authentication/view_models/signup_vm.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tiktok_flutter/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_flutter/features/onboarding/interests_screen.dart';
+import 'package:tiktok_flutter/features/users/view_models/users_vm.dart';
 import 'package:tiktok_flutter/utils.dart';
 
 class SignUpViewModel extends AsyncNotifier<void> {
@@ -17,12 +18,15 @@ class SignUpViewModel extends AsyncNotifier<void> {
   Future<void> signUp(BuildContext context) async {
     state = const AsyncValue.loading();
     final form = ref.read(signUpForm);
-    state = await AsyncValue.guard(
-      () async => await _authRepo.emailSignUp(
+    final users = ref.read(usersProvider.notifier);
+    state = await AsyncValue.guard(() async {
+      final userCredential = await _authRepo.emailSignUp(
         form['email'],
         form['password'],
-      ),
-    );
+      );
+      await users.createProfile(userCredential);
+    });
+
     if (state.hasError) {
       showFirebaseErrorSnack(context, state.error);
     } else {
